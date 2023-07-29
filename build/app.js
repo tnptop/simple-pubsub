22622a141dca59668360084c313cd546d145e732
Thanapat Bhunnachet <me@tnptop.com>
2023-07-29 10:56:26 +0700

    Add unsubscribe(type, handler)

--- build/app.js
+++ build/app.js
@@ -41,24 +41,36 @@ class MachineRefillEvent {
 }
 class MachineSaleSubscriber {
     constructor(machines) {
+        this._id = uuidv4();
         this.machines = machines;
+    }
+    subscriberId() {
+        return this._id;
     }
     handle(event) {
         const machineId = event.machineId();
         const machineIndex = this.machines.findIndex(machine => machine.id === machineId);
-        this.machines[machineIndex].stockLevel -= event.getSoldQuantity();
-        console.log('sale', this.machines);
+        const oldStockLevel = this.machines[machineIndex].stockLevel;
+        const newStockLevel = oldStockLevel - event.getSoldQuantity();
+        this.machines[machineIndex].stockLevel = newStockLevel;
+        console.log('MachineSaleEvent - Subscriber ID:', this._id, ', Machine ID:', machineId, ', sold quantity:', event.getSoldQuantity(), ', old stock level:', oldStockLevel, ', new stock level:', newStockLevel);
     }
 }
 class MachineRefillSubscriber {
     constructor(machines) {
+        this._id = uuidv4();
         this.machines = machines;
+    }
+    subscriberId() {
+        return this._id;
     }
     handle(event) {
         const machineId = event.machineId();
         const machineIndex = this.machines.findIndex(machine => machine.id === machineId);
-        this.machines[machineIndex].stockLevel += event.getRefillQuantity();
-        console.log('refill', this.machines);
+        const oldStockLevel = this.machines[machineIndex].stockLevel;
+        const newStockLevel = oldStockLevel + event.getRefillQuantity();
+        this.machines[machineIndex].stockLevel = newStockLevel;
+        console.log('MachineRefillEvent - Subscriber ID:', this._id, ', Machine ID:', machineId, ', refill quantity:', event.getRefillQuantity(), ', old stock level:', oldStockLevel, ', new stock level:', newStockLevel);
     }
 }
 class MachinePublishSubscribeService {
@@ -75,6 +87,13 @@ class MachinePublishSubscribeService {
         var _a;
         const handlers = (_a = this.eventSubscriberMap.get(type)) !== null && _a !== void 0 ? _a : [];
         this.eventSubscriberMap.set(type, handlers.concat(handler));
+    }
+    unsubscribe(type, handler) {
+        var _a;
+        let handlers = (_a = this.eventSubscriberMap.get(type)) !== null && _a !== void 0 ? _a : [];
+        const handlerIndex = handlers.findIndex(activeHandler => activeHandler.subscriberId() === handler.subscriberId());
+        handlers.splice(handlerIndex, 1);
+        this.eventSubscriberMap.set(type, handlers);
     }
 }
 // objects
@@ -104,6 +123,17 @@ const eventGenerator = () => {
     const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
     return new MachineRefillEvent(refillQty, randomMachine());
 };
+const uuidv4 = () => {
+    const uuid = new Array(36);
+    for (let i = 0; i < 36; i++) {
+        uuid[i] = Math.floor(Math.random() * 16);
+    }
+    uuid[14] = 4;
+    uuid[19] = uuid[19] &= ~(1 << 2);
+    uuid[19] = uuid[19] |= (1 << 3);
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+    return uuid.map((x) => x.toString(16)).join('');
+};
 // program
 (() => __awaiter(void 0, void 0, void 0, function* () {
     // create 3 machines with a quantity of 10 stock
@@ -111,12 +141,17 @@ const eventGenerator = () => {
     // create a machine sale event subscriber. inject the machines (all subscribers should do this)
     const saleSubscriber = new MachineSaleSubscriber(machines);
     const refillSubscriber = new MachineRefillSubscriber(machines);
+    const anotherSaleSubscriber = new MachineSaleSubscriber(machines);
     // create the PubSub service
     const pubSubService = new MachinePublishSubscribeService();
     pubSubService.subscribe('sale', saleSubscriber);
+    pubSubService.subscribe('sale', anotherSaleSubscriber);
     pubSubService.subscribe('refill', refillSubscriber);
+    console.log(pubSubService);
     // create 5 random events
     const events = [1, 2, 3, 4, 5].map(i => eventGenerator());
     // publish the events
     events.map(event => pubSubService.publish(event));
+    pubSubService.unsubscribe('sale', anotherSaleSubscriber);
+    console.log(pubSubService);
 }))();
--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,13 @@ interface IEvent {
 
 interface ISubscriber {
   handle(event: IEvent): void;
+  subscriberId(): string;
 }
 
 interface IPublishSubscribeService {
   publish (event: IEvent): void;
   subscribe (type: string, handler: ISubscriber): void;
-  // unsubscribe ( /* Question 2 - build this feature */ );
+  unsubscribe (type: string, handler: ISubscriber): void;
 }
 
 
@@ -50,35 +51,51 @@ class MachineRefillEvent implements IEvent {
 
 class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
+  private readonly _id: string;
 
   constructor (machines: Machine[]) {
+    this._id = uuidv4();
     this.machines = machines; 
+  }
+
+  subscriberId(): string {
+    return this._id;
   }
 
   handle(event: MachineSaleEvent): void {
     const machineId = event.machineId();
     const machineIndex = this.machines.findIndex(machine => machine.id === machineId);
+    const oldStockLevel = this.machines[machineIndex].stockLevel;
+    const newStockLevel = oldStockLevel - event.getSoldQuantity();
 
-    this.machines[machineIndex].stockLevel -= event.getSoldQuantity();
-    
-    console.log('sale', this.machines);
+    this.machines[machineIndex].stockLevel = newStockLevel;
+
+    console.log('MachineSaleEvent - Subscriber ID:', this._id, ', Machine ID:', machineId, ', sold quantity:', event.getSoldQuantity(), ', old stock level:', oldStockLevel, ', new stock level:', newStockLevel);
   }
 }
 
 class MachineRefillSubscriber implements ISubscriber {
   public machines: Machine[];
+  private readonly _id: string;
 
   constructor (machines: Machine[]) {
+    this._id = uuidv4();
     this.machines = machines;
+  }
+
+  subscriberId(): string {
+    return this._id;
   }
 
   handle(event: MachineRefillEvent): void {
     const machineId = event.machineId();
     const machineIndex = this.machines.findIndex(machine => machine.id === machineId);
+    const oldStockLevel = this.machines[machineIndex].stockLevel;
+    const newStockLevel = oldStockLevel + event.getRefillQuantity();
 
-    this.machines[machineIndex].stockLevel += event.getRefillQuantity();
-    
-    console.log('refill', this.machines);
+    this.machines[machineIndex].stockLevel = newStockLevel;
+
+    console.log('MachineRefillEvent - Subscriber ID:', this._id, ', Machine ID:', machineId, ', refill quantity:', event.getRefillQuantity(), ', old stock level:', oldStockLevel, ', new stock level:', newStockLevel);
   }
 }
 
@@ -99,6 +116,16 @@ class MachinePublishSubscribeService implements IPublishSubscribeService {
   subscribe(type: string, handler: ISubscriber): void {
     const handlers: ISubscriber[] = this.eventSubscriberMap.get(type) ?? [];
     this.eventSubscriberMap.set(type, handlers.concat(handler));
+  }
+
+  unsubscribe(type: string, handler: ISubscriber): void {
+    let handlers: ISubscriber[] = this.eventSubscriberMap.get(type) ?? [];
+    const handlerIndex = handlers.findIndex(
+      activeHandler => activeHandler.subscriberId() === handler.subscriberId()
+    );
+    handlers.splice(handlerIndex, 1);
+
+    this.eventSubscriberMap.set(type, handlers);
   }
 }
 
@@ -136,6 +163,18 @@ const eventGenerator = (): IEvent => {
   return new MachineRefillEvent(refillQty, randomMachine());
 }
 
+const uuidv4 = (): string => {
+  const uuid = new Array(36);
+  for (let i = 0; i < 36; i++) {
+    uuid[i] = Math.floor(Math.random() * 16);
+  }
+  uuid[14] = 4;
+  uuid[19] = uuid[19] &= ~(1 << 2);
+  uuid[19] = uuid[19] |= (1 << 3);
+  uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+  return uuid.map((x) => x.toString(16)).join('');  
+}
+
 
 // program
 (async () => {
@@ -143,8 +182,8 @@ const eventGenerator = (): IEvent => {
   const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
-  const saleSubscriber = new MachineSaleSubscriber(machines);
-  const refillSubscriber = new MachineRefillSubscriber(machines);
+  const saleSubscriber: MachineSaleSubscriber = new MachineSaleSubscriber(machines);
+  const refillSubscriber: MachineRefillSubscriber = new MachineRefillSubscriber(machines);
 
   // create the PubSub service
   const pubSubService: MachinePublishSubscribeService = new MachinePublishSubscribeService();
