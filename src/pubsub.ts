8e6c11764e9e8ea3de31f989681b34ccb3bca4bd
Thanapat Bhunnachet <me@tnptop.com>
2023-07-29 20:37:16 +0700

    Revert to instant execution

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -8,55 +8,16 @@ interface IPublishSubscribeService {
 }
 
 class PublishSubscribeService implements IPublishSubscribeService {
-  private eventQueues: Map<string, IEvent[]>;
   private eventSubscriberMap: Map<string, ISubscriber[]>;
 
   constructor() {
-    this.eventQueues = new Map<string, IEvent[]>();
     this.eventSubscriberMap = new Map<string, ISubscriber[]>();
-
-    // start eventQueue dispatcher - processing 1 event per second
-    setInterval(() => this.#execute(), 1000);
   }
 
-  #execute(): void {
-    // get the first event in all queues
-    let eventToBeExecuted: IEvent | undefined;
-    let eventQueueToBeUnqueue: string = '';
-    this.eventQueues.forEach(eventQueue => {
-      const event: IEvent | undefined = eventQueue[0];
-
-      if (event && (!eventToBeExecuted || event.timestamp < eventToBeExecuted.timestamp)) {
-        eventToBeExecuted = event;
-        eventQueueToBeUnqueue = event.type();
-      }
-    }, this);
-
-    if (eventToBeExecuted && eventQueueToBeUnqueue) {
-      const event: IEvent = eventToBeExecuted;
-      const eventType = event.type();
-      const eventQueue: IEvent[] = this.eventQueues.get(eventType) ?? [];
-      const handlers: ISubscriber[] = this.eventSubscriberMap.get(event.type()) ?? [];
-
-      // unqueue the event to be executed
-      this.eventQueues.set(eventType, eventQueue.slice(1));
-      handlers.forEach(handler => handler.handle(event));
-    }
-  }
-
-  publish(event: IEvent): void {
-    const eventType = event.type();
-    const eventQueue: IEvent[] = this.eventQueues.get(eventType) ?? [];
-    this.eventQueues.set(eventType, eventQueue.concat(event));
-  }
-
-  /*
-  // instant event execution
   publish(event: IEvent): void {
     const handlers: ISubscriber[] = this.eventSubscriberMap.get(event.type()) ?? [];
     handlers.forEach(handler => handler.handle(event));
   }
-  */
 
   subscribe(type: string, handler: ISubscriber): void {
     const handlers: ISubscriber[] = this.eventSubscriberMap.get(type) ?? [];
